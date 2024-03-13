a97956ca3257a6643c2ea48f7832f4e0f94a3cf7
Luigi <luigicruz.dev@gmail.com>
2024-03-13 09:34:07 +0800

    update breeds service

--- src/services/breeds.ts
+++ src/services/breeds.ts
@@ -11,3 +11,23 @@ export async function getBreeds() {
     console.error(error);
   }
 }
+
+export async function getBreed(breedId: string) {
+  return axios.get(`/breeds/${breedId}`);
+}
+
+export async function getBreedFacts(breedId: string) {
+  return axios.get(`/breeds/${breedId}/facts`);
+}
+
+export async function searchBreeds(query: string) {
+  return axios.get(`/breeds/search?q=${query}&attach_image=1`);
+}
+
+export async function getBreedImage(breedId: string) {
+  return axios.get(`/images/search?page=1&limit=10&breed_id=${breedId}`);
+}
+
+export async function getCatDetail(catId: string) {
+  return axios.get(`/images/${catId}`);
+}
