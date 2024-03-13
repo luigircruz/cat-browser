f992e7aa1478d824c40ef16f661453a4cd9aca14
Luigi <luigicruz.dev@gmail.com>
2024-03-13 09:30:59 +0800

    create axios util

---
+++ src/utils/axios.ts
@@ -0,0 +1,15 @@
+import Axios from "axios";
+
+const baseURL = "https://api.thecatapi.com/v1";
+
+const axios = Axios.create({
+  baseURL,
+  headers: {
+    "X-Requested-With": "XMLHttpRequest",
+    "X-Client-Platform": "Web",
+    "x-api-key": import.meta.env.REACT_APP_CAT_API_KEY,
+  },
+  timeout: 60000,
+});
+
+export default axios;
